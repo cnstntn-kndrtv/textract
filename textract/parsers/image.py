055726d24ad3b79dafa982150905531b6d278f67
cnstntn <cnstntn.kndrtv@gmail.com>
2023-05-23 15:17:57 +0500

    class destructor

--- textract/parsers/image.py
+++ textract/parsers/image.py
@@ -28,13 +28,15 @@ class Parser(ShellParser):
 
     def __init__(self,
                  use_gpu: bool = False,
-                 delete_reader: bool = True,
                  ) -> None:
 
         self.use_gpu = use_gpu
-        self.delete_reader = delete_reader
         self.reader = self._init_reader()
         super().__init__()
+
+    def __del__(self):
+        """Destructor."""
+        del self.reader
 
     def _init_reader(self) -> easyocr.Reader:
         return easyocr.Reader(['ru','en'], gpu=self.use_gpu)
@@ -43,8 +45,5 @@ class Parser(ShellParser):
 
         result = self.reader.readtext(filename, paragraph=True, y_ths=-0.1)
         text = '\n'.join([r[1] for r in result])
-
-        if self.delete_reader:
-            del self.reader
 
         return text
